aab6cd4d0cc8fc66b97d20f60946dcb5f32cbc89
SamWang-company <wds0405@qq.com>
2016-02-03 15:43:54 +0800

    update config buildToolsVersion = "23.0.1"

--- lib/src/main/java/com/sam/lib/utils/NotificationUtils.java
+++ lib/src/main/java/com/sam/lib/utils/NotificationUtils.java
@@ -1,43 +1,34 @@
 package com.sam.lib.utils;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 public class NotificationUtils {
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public static void showNotification(Context context,Class cls,String title,String content,int iconResId){
 
-        Notification n = new Notification();
-        n.icon = iconResId;
-        n.tickerText = AppUtils.getAppName(context);
-        n.when = System.currentTimeMillis();
-        //n.flags=Notification.FLAG_ONGOING_EVENT;
-        Intent intent = new Intent(context, cls);
-        PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
-        n.setLatestEventInfo(context, title,content, pi);
-
-        String service = Context.NOTIFICATION_SERVICE;
-        NotificationManager nm = (NotificationManager) context.getSystemService(service);
-        nm.notify(1, n);
-
-/* api 16
+//api 16
         Notification.Builder builder = new Notification.Builder(context);
-        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setSmallIcon(iconResId);
         builder.setTicker(AppUtils.getAppName(context));
         builder.setWhen( System.currentTimeMillis());
         //n.flags=Notification.FLAG_ONGOING_EVENT;
         Intent intent = new Intent(context, cls);
         PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
 //        builder. .setLatestEventInfo(context, title, content, pi);
-        builder.setContentIntent(pi);
-        builder.setContentTitle(title);
+        builder.setContentIntent(pi)
+                .setContentTitle(title)
+                .setContentText(content);
 
         String service = Context.NOTIFICATION_SERVICE;
         NotificationManager nm = (NotificationManager) context.getSystemService(service);
-        nm.notify(1, builder.build());*/
+        nm.notify(1, builder.build());
 
     }
 }
